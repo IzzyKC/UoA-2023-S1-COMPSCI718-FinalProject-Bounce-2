48937e66abbf03fe900923880c1310a063f52073
changwanling <wcha305@aucklanduni.ac.nz>
2023-06-10 12:50:29 +1200

    task1 after pass Test task1

--- src/bounce/views/Task1.java
+++ src/bounce/views/Task1.java
@@ -1,55 +1,90 @@
 package bounce.views;
 
+import bounce.NestingShape;
+import bounce.Shape;
 import bounce.ShapeModel;
 
 import javax.swing.event.TreeModelListener;
 import javax.swing.tree.TreeModel;
 import javax.swing.tree.TreePath;
+import java.util.ArrayList;
 
 /*
  * TODO Complete this class to display a ShapeModel's shape composition on the JTree
  */
 public class Task1 implements TreeModel {
+    private ShapeModel _adaptee;
 
     public Task1(ShapeModel shapeModel) {
+        this._adaptee = shapeModel;
 
     }
 
     @Override
     public Object getRoot() {
-        return null;
+        return _adaptee.root();
     }
 
     @Override
     public Object getChild(Object parent, int index) {
+        try{
+            if(parent instanceof NestingShape){
+                return ((NestingShape) parent).shapeAt(index);
+            }
+        }catch(IndexOutOfBoundsException e){
+            System.out.println("Index error: " + e.getMessage());
+        }
         return null;
     }
 
     @Override
     public int getChildCount(Object parent) {
+        if(parent instanceof NestingShape)
+            return ((NestingShape) parent).shapeCount();
         return 0;
     }
 
+    /**
+     * NestingShape can have children, return false;
+     * Otherwise, return true(is a leaf)
+     * @param node  a node in the tree, obtained from this data source
+     * @return is a Leaf node
+     */
     @Override
     public boolean isLeaf(Object node) {
-        return false;
+        return !(node instanceof NestingShape);
     }
 
     @Override
     public void valueForPathChanged(TreePath path, Object newValue) {
-
+        //simply be implemented with an empty method body.
+        //This method is an event notification method that can safely be implemented like this
+        //in the context of Task 1.
     }
 
     @Override
     public int getIndexOfChild(Object parent, Object child) {
-        return 0;
+        if(parent == null || child == null) return -1;
+        if(parent instanceof NestingShape){
+            Shape childShape = (child instanceof Shape) ? (Shape) child : null;
+            return ((NestingShape) parent).indexOf(childShape);
+        }
+        return -1;
     }
 
+    /**
+     *Adds a listener for the TreeModelEvent posted after the tree changes.
+     * @param l       the listener to add
+     */
     @Override
     public void addTreeModelListener(TreeModelListener l) {
 
     }
 
+    /**
+     * Removes a listener previously added with addTreeModelListener
+     * @param l       the listener to remove
+     */
     @Override
     public void removeTreeModelListener(TreeModelListener l) {
 
